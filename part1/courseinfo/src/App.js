a343d7903a1f616d13403b76ae8aaa37662a21de
Kartikay <kartiksrivastava19@gmail.com>
2023-03-21 17:29:58 +0530

    Done exercise1.5 course information

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -44,27 +44,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
-  const course = "Half Stack application development";
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <>
-      <Header course={course} />
-      <Content part={parts} />
-      <Total part={parts} />
+      <Header course={course.name} />
+      <Content part={course.parts} />
+      <Total part={course.parts} />
     </>
   );
 };
