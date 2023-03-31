9c197838b701bef43b02d6d1abd7e8a841467d87
Kartikay <kartiksrivastava19@gmail.com>
2023-04-01 01:52:16 +0530

    Add courseinfo step9 exercise2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -11,13 +11,11 @@ const Part = (props) => {
   );
 };
 
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => (
-      <Part key={part.id} name={part.name} exercise={part.exercises} />
-    ))}
-  </>
-);
+const Content = ({ parts }) => {
+  return parts.map((part) => (
+    <Part key={part.id} name={part.name} exercise={part.exercises} />
+  ));
+};
 
 const Header = ({ header }) => {
   return <h1>{header}</h1>;
@@ -34,29 +32,47 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return courses.map((course) => <Course key={course.id} course={course} />);
 };
 
 export default App;
