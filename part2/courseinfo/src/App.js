f792741776bcb2be3f8e329608670a569832c692
Kartikay <kartiksrivastava19@gmail.com>
2023-03-31 22:31:50 +0530

    Add courseinfo step6 exercise2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,54 @@
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercise}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return parts.map((part) => (
+    <Part key={part.id} name={part.name} exercise={part.exercises} />
+  ));
+};
+
+const Header = ({ header }) => {
+  return <h1>{header}</h1>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header header={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
