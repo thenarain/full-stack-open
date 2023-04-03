0e9ce4ec0bd04441fc4497c4b8e7d75ff5a986b5
Kartikay <kartiksrivastava19@gmail.com>
2023-04-03 15:38:30 +0530

    Add Phonebook step3 exercise2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040 - 1234577" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -12,15 +15,21 @@ const App = () => {
     } else {
       const newobj = {
         name: newName,
+        number: newNumber,
       };
       setPersons(persons.concat(newobj));
     }
 
     setNewName("");
+    setNewNumber("");
   };
 
   const newNoteChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const newNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -31,13 +40,18 @@ const App = () => {
           name: <input value={newName} onChange={newNoteChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={newNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => (
-          <p key={person.name}>{person.name}</p>
+          <p key={person.name}>
+            {person.name} {person.number}{" "}
+          </p>
         ))}
       </div>
     </div>
